b0d40f207afb515376e07c1670e182c6a4636bfa
Sebastian-Quast <Strike.in@googlemail.com>
2018-10-21 11:36:57 +0200

    First commit

---
+++ src/main/java/MyMapDataHandler.java
@@ -0,0 +1,24 @@
+import de.westnordost.osmapi.map.data.BoundingBox;
+import de.westnordost.osmapi.map.data.Relation;
+import de.westnordost.osmapi.map.data.Way;
+import de.westnordost.osmapi.map.handler.MapDataHandler;
+
+public class MyMapDataHandler implements MapDataHandler {
+
+
+    public void handle(BoundingBox boundingBox) {
+
+    }
+
+    public void handle(de.westnordost.osmapi.map.data.Node node) {
+
+    }
+
+    public void handle(Way way) {
+
+    }
+
+    public void handle(Relation relation) {
+
+    }
+}
